377c84fc031c7e0edb2f55ebdee6a3934a90dbc3
Oteng-Derrick <42379936+derrique12@users.noreply.github.com>
2022-09-19 14:22:27 +0000

    change

--- lib/service/firebase_db.dart
+++ lib/service/firebase_db.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:sedweb/models/user_model.dart';
 
 class FirebaseDB {
